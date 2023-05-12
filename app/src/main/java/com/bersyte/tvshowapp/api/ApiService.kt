b1678b03c43b6fad1d998bb52ca57f6addad1760
hhyeok1026 <hhyeok1026@gmail.com>
2023-05-13 03:39:31 +0900

    영상보기전 소스코드 제거

--- app/src/main/java/com/bersyte/tvshowapp/api/ApiService.kt
+++ app/src/main/java/com/bersyte/tvshowapp/api/ApiService.kt
@@ -6,8 +6,3 @@ import retrofit2.Response
 import retrofit2.http.GET
 
 interface ApiService {
-
-    @GET(Constants.END_POINT)
-    suspend fun getTvShows():Response<TvShowResponse>
-
-}
--- app/src/main/java/com/bersyte/tvshowapp/repository/TvShowRepository.kt
+++ app/src/main/java/com/bersyte/tvshowapp/repository/TvShowRepository.kt
@@ -3,8 +3,6 @@ package com.bersyte.tvshowapp.repository
 import com.bersyte.tvshowapp.api.ApiService
 import javax.inject.Inject
 
-class TvShowRepository
-@Inject
-constructor(private val apiService: ApiService) {
-    suspend fun getTvShows() = apiService.getTvShows()
+class TvShowRepository {
+
 }
